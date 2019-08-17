201552148bad6f3547d0bcda9cdf2d123bfee94a
Renato <renatosilveira99@hotmail.com>
2019-08-17 14:13:10 -0300

    chatlist iniciado

--- src/pages/ChatList.js
+++ src/pages/ChatList.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
+import styles from '../assets/ChatList';
+
 export default class ChatList extends Component {
   render() {
     return (
       <View>
-        <Text> Tela de Lista de Chats (Luiz Gustavo) </Text>
-     
+          <Text>Tela de Chat</Text>
       </View>
     )
   }
