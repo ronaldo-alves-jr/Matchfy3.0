aa4d19028acbc785427762e60089174b3c89040d
Renato <renatosilveira99@hotmail.com>
2019-08-31 01:07:34 -0300

    tela de interesses em branco, devido a problemas de integração

--- src/pages/Interest.js
+++ src/pages/Interest.js
@@ -1,31 +1,14 @@
-import React from 'react';
-import {
-  View,
-  TouchableOpacity,
-  Text
-} from 'react-native';
-import ReactNativeSettingsPage, { 
-  NavigateRow,
-  SwitchRow, 
-  SectionRow
-} from 'react-native-settings-page';
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import Slider from '@react-native-community/slider';
-
-import {List, ListItem, Container, Content} from 'native-base';
-
-export default class Interest extends React.Component {
+// create a component
+export default class Interest extends Component {
   render() {
     return (
-      <View>
-          <Slider
-    style={{width: 200, height: 40}}
-    minimumValue={0}
-    maximumValue={1}
-    minimumTrackTintColor="#FFFFFF"
-    maximumTrackTintColor="#000000"
-  />
+      <View style={{backgroundColor: "black"}}>
+        <Text style={{color: "white"}}>Tela de Intereses</Text>
       </View>
-    )
+    );
   }
 }
